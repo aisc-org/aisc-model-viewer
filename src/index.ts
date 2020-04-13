bfd6ecfbc4bd3f49069220cf9b8b7d392ab94b6f
Peter Talley <peterctalley@gmail.com>
2020-04-13 10:06:02 -0400

    Add lights

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,12 @@ let scene = new THREE.Scene()
 let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 let renderer = new THREE.WebGLRenderer({ antialias: true });
 
-scene.background = new THREE.Color(0xCCCCCC)
+let ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.5)
+let directionalLight = new THREE.DirectionalLight(0xFFFFFF, 0.5)
+directionalLight.position.set(0.5, 0.5, 0.5)
+scene.add(ambientLight)
+scene.add(directionalLight)
+scene.background = new THREE.Color(0x000000)
 
 renderer.setPixelRatio(window.devicePixelRatio)
 renderer.setSize(window.innerWidth, window.innerHeight)
