ca4e5da33078c71fe9d66ab17c570d4f3c66344b
Peter Talley <peterctalley@gmail.com>
2020-04-27 12:02:14 -0400

    Split viewer out into class

--- src/index.ts
+++ src/index.ts
@@ -1,70 +1,17 @@
-import * as THREE from 'three'
-import { GLTFLoader, GLTF } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import modelPath from './neyland-connection.gltf'
+import { Model, ModelViewer } from './viewer'
+import pathToDoubleAngle from './neyland-connection.gltf'
 import './style.css'
 
-// Set up the renderer
-let scene = new THREE.Scene()
-let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-let renderer = new THREE.WebGLRenderer({ antialias: true });
+const modelList = [
+    new Model('Double Angle Connection', pathToDoubleAngle)
+]
 
-let ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.5)
-let directionalLight = new THREE.DirectionalLight(0xFFFFFF, 0.5)
-directionalLight.position.set(0.5, 0.5, 0.5)
-scene.add(ambientLight)
-scene.add(directionalLight)
-scene.background = new THREE.Color(0x000000)
+// Set up the page
+const container = document.createElement('div')
+container.className = 'viewer-container'
+document.body.appendChild(container)
 
-renderer.setPixelRatio(window.devicePixelRatio)
-renderer.setSize(window.innerWidth, window.innerHeight)
-renderer.toneMapping = THREE.ACESFilmicToneMapping
-renderer.toneMappingExposure = 0.8
-renderer.outputEncoding = THREE.sRGBEncoding
-document.body.appendChild(renderer.domElement)
+const viewer = new ModelViewer(container)
+viewer.setModelAsCurrent(modelList[0])
 
-// Controls
-let controls = new OrbitControls(camera, renderer.domElement)
-controls.addEventListener('change', render)
-controls.update()
-
-// Load the model
-const loader = new GLTFLoader()
-loader.load(modelPath, function(gltf) {
-    const box = new THREE.Box3().setFromObject(gltf.scene)
-    const size = box.getSize(new THREE.Vector3()).length()
-    const center = box.getCenter(new THREE.Vector3())
-
-    controls.reset()
-
-    gltf.scene.position.x += (gltf.scene.position.x - center.x)
-    gltf.scene.position.y += (gltf.scene.position.y - center.y)
-    gltf.scene.position.z += (gltf.scene.position.z - center.z)
-
-    controls.maxDistance = size*10
-    camera.near = size / 100
-    camera.far = size * 100
-    camera.updateProjectionMatrix()
-
-    camera.position.copy(center)
-    camera.position.x += size / 2.0
-    camera.position.y += size / 5.0
-    camera.position.z += size / 2.0
-    camera.lookAt(center)
-
-    scene.add(gltf.scene)
-    render()
-})
-
-window.addEventListener('resize', onWindowResize, false)
-
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    render()
-}
-
-function render() {
-    renderer.render(scene, camera)
-}
+window.addEventListener('resize', viewer.onWindowResize.bind(viewer), false)
---
+++ src/viewer.ts
@@ -0,0 +1,112 @@
+import * as THREE from 'three'
+import { GLTFLoader, GLTF } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
+
+/**
+ * Metadata wrapper around the standard GLTF object.
+ *
+ * @param name - The name of the model
+ * @param gltf - The actual GLTF object
+ */
+export class Model {
+    name: string
+    path: string
+    gltf: GLTF
+
+    /**
+     * @param name - The name to use in the sidebar for this model
+     * @param path - The path to the GLTF file for this model
+     */
+    constructor(name: string, path: string) {
+        this.name = name
+        this.path = path
+    }
+}
+
+
+export class ModelViewer {
+    container: HTMLElement
+    loader: GLTFLoader
+
+    scene: THREE.Scene
+    camera: THREE.PerspectiveCamera
+    renderer: THREE.WebGLRenderer
+    controls: OrbitControls
+
+    constructor(container: HTMLElement) {
+        this.container = container
+
+        this.loader = new GLTFLoader()
+        this.loader.setPath('/')
+
+        this.scene = new THREE.Scene()
+        this.addLights()
+        this.camera = new THREE.PerspectiveCamera(75, this.container.clientWidth / this.container.clientHeight)
+
+        this.renderer = new THREE.WebGLRenderer({ antialias: true })
+        this.renderer.setPixelRatio(window.devicePixelRatio)
+        this.renderer.setSize(this.container.clientWidth, this.container.clientHeight)
+        this.renderer.outputEncoding = THREE.sRGBEncoding
+
+        this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+        this.controls.addEventListener('change', this.render.bind(this))
+        this.controls.update()
+
+        this.container.appendChild(this.renderer.domElement)
+        this.render()
+    }
+
+    setModelAsCurrent(model: Model) {
+        this.clearScene()
+        this.loader.load(model.path, (gltf) =>{
+            // Determine the size of the model, and move it to the center of the scene
+            const box = new THREE.Box3().setFromObject(gltf.scene)
+            const size = box.getSize(new THREE.Vector3()).length()
+            const center = box.getCenter(new THREE.Vector3())
+    
+            gltf.scene.position.x += (gltf.scene.position.x - center.x)
+            gltf.scene.position.y += (gltf.scene.position.y - center.y)
+            gltf.scene.position.z += (gltf.scene.position.z - center.z)
+    
+            this.controls.reset()
+            this.controls.maxDistance = size*10
+            this.camera.near = size / 100
+            this.camera.far = size * 100
+            this.camera.updateProjectionMatrix()
+    
+            this.camera.position.copy(center)
+            this.camera.position.x += size / 2.0
+            this.camera.position.y += size / 5.0
+            this.camera.position.z += size / 2.0
+            this.camera.lookAt(center)
+    
+            this.scene.add(gltf.scene)
+        })
+        this.render()
+    }
+
+    addLights() {
+        const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.5)
+        const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 0.5)
+        directionalLight.position.set(1, 1, 1)
+
+        this.scene.add(ambientLight, directionalLight)
+    }
+
+    clearScene() {
+        this.scene.traverse(function (child) {
+        })
+    }
+
+    onWindowResize() {
+        this.camera.aspect = this.container.clientWidth / this.container.clientHeight
+        this.camera.updateProjectionMatrix()
+        this.renderer.setSize(this.container.clientWidth, this.container.clientHeight)
+        this.render()
+    }
+
+    render() {
+        this.renderer.render(this.scene, this.camera)
+    }
+}
