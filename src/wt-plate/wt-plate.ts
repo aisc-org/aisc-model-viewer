59a851b54bc92f22988a26f99e7beedd3bd52a6a
Peter Talley <peterctalley@gmail.com>
2020-07-09 20:52:12 -0400

    Improvements to WT model
    - Bolts are more correct:
        - Added washers
        - Flipped orientation so that threads are excluded from shear plane
        - Bolt head is correctly detailed (only one bevel)
        - Bevels are redone to be correctly proportioned
        - Nut is no longer solid, makes clear that it is the nut
    - Flipped connection up/down so WT is on top
    - Added fillet
    - Tensile yield no longer shows the WT as broken
    - Added more failure modes:
        - Tensile rupture
        - Block shear 1, 2, and 3

--- src/wt-plate/wt-plate.ts
+++ src/wt-plate/wt-plate.ts
@@ -1,6 +1,10 @@
 import { App, Model } from '../app'
 import pathToModel from './WT Connection.glb'
-import pathToYield from './WT Connection - Tee yield.glb'
+import pathToRupture from './WT Connection -- Tensile rupture.glb'
+import pathToWebShear from './WT Connection -- Web block shear.glb'
+import pathToIntuitiveShear from './WT Connection -- Intuitive block shear.glb'
+import pathToUnlikelyShear from './WT Connection -- Unlikely block shear.glb'
+import pathToYield from './WT Connection -- Tensile yield.glb'
 
 const app = new App({
     title: 'WT bolted to plate',
@@ -9,7 +13,11 @@ const app = new App({
             name: 'models',
             items: [
                 new Model({name: '3D model', path: pathToModel}),
-                new Model({name: 'Tensile yield', path: pathToYield})
+                new Model({name: 'Tensile rupture', path: pathToRupture}),
+                new Model({name: 'Web block shear', path: pathToWebShear}),
+                new Model({name: 'Block shear 2', path: pathToIntuitiveShear}),
+                new Model({name: 'Block shear 3', path: pathToUnlikelyShear}),
+                new Model({name: 'Tensile yield', path: pathToYield}),
             ]
         },
     ]
