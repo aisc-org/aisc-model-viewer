3280195248db0cb3f11b249a255b5df52d40ce57
Peter Talley <peterctalley@gmail.com>
2020-07-27 15:26:28 -0400

    Add barebones more fun doc

---
+++ src/docs/adding-more-fun.md
@@ -0,0 +1,16 @@
+
+[This is a link to AISC](https://www.aisc.org).
+
+[This is a link to a page in this collection](./#Calculations). The URL is
+determined by the `name` set in the `.ts` file. Just replace any spaces with
+hyphens. E.g. `name: 'Block shear rupture'` will be linked to at
+`./#Block-shear-rupture`.
+
+Separate sections by `<hr>`s:
+
+--------------------------------------------------------------------------------
+
+![This creates a figure. If the image is not already declared, it needs to be added to the appropriate `.ts` file.](../assets/placeholder.png)
+
+To declare an image, just add `import 'relative/path/to/image'` to the
+appropriate `.ts` file.
--- src/docs/docs.ts
+++ src/docs/docs.ts
@@ -1,9 +1,12 @@
 import { App, HtmlItem } from '../app'
 
+// Content
 import devEnvironment from './dev-environment.md'
 import addingAModel from './adding-a-model.md'
 import exportingToGltf from './exporting-to-gltf.md'
+import addingMoreFun from './adding-more-fun.md'
 
+// Images
 import './tools-for-native-modules.png'
 
 const app = new App({
@@ -15,6 +18,7 @@ const app = new App({
                 new HtmlItem({ name: 'Setting up a development environment', content: devEnvironment }),
                 new HtmlItem({ name: 'Adding a model', content: addingAModel }),
                 new HtmlItem({ name: 'Exporting to glTF', content: exportingToGltf }),
+                new HtmlItem({ name: 'Adding a more fun item', content: addingMoreFun }),
             ]
         }
     ]
