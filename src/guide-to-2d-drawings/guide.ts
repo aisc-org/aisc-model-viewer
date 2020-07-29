eb3fa5e0986dd106b474ab38139aab6483ff1f18
Peter Talley <peterctalley@gmail.com>
2020-07-29 09:05:25 -0400

    Add guide content to page

--- src/guide-to-2d-drawings/guide.ts
+++ src/guide-to-2d-drawings/guide.ts
@@ -2,6 +2,10 @@ import { App, HtmlItem } from '../app'
 
 // Content
 import guideLanding from './guide-to-2d-drawings.md'
+import typesOfDrawings from './types-of-drawings.md'
+import linework from './linework.md'
+import boltsAndWelds from './bolts-and-welds.md'
+import abbreviations from './abbreviations.md'
 
 // Images/assets
 // import '/relative/path/to/asset.ext'
@@ -14,6 +18,10 @@ new App({
             name: '',
             items: [
                 new HtmlItem({ name: 'Landing', content: guideLanding }),
+                new HtmlItem({ name: 'Types of drawings', content: typesOfDrawings }),
+                new HtmlItem({ name: 'Linework', content: linework }),
+                new HtmlItem({ name: 'Bolts and welds', content: boltsAndWelds }),
+                new HtmlItem({ name: 'Abbreviations', content: abbreviations }),
             ]
         }
     ],
