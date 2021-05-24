b62409cc7b458e616f390dc9f98828f4cf569a3c
Peter Talley <peterctalley@gmail.com>
2021-05-24 01:17:18 -0400

    Add lateral-torsional buckling model

--- src/buckling-i-shaped-beam/buckling-i-shaped-beam.ts
+++ src/buckling-i-shaped-beam/buckling-i-shaped-beam.ts
@@ -5,6 +5,7 @@ import pathToFlangeLB from './I-shaped-beam-Flange-LB.glb'
 import pathToWebLB from './I-shaped-beam-Web-LB.glb'
 import pathToShearBuckling1 from './I-shaped-beam-Shear-buckling-no-stiffeners.glb'
 import pathToShearBuckling2 from './I-shaped-beam-Shear-buckling-with-stiffeners.glb'
+import pathToLTB from './I-shaped-beam-Lateral-torsional-buckling.glb'
 
 new App({
     title: 'I-Shaped Beam',
@@ -16,6 +17,7 @@ new App({
                 new Model({ name: 'Web local buckling', path: pathToWebLB }),
                 new Model({ name: 'Shear buckling (no stiffeners)', path: pathToShearBuckling1, maxScale: 20 }),
                 new Model({ name: 'Shear buckling (with stiffeners)', path: pathToShearBuckling2 }),
+                new Model({ name: 'Lateral-torsional buckling', path: pathToLTB, maxScale: 50 }),
             ]
         },
     ],
