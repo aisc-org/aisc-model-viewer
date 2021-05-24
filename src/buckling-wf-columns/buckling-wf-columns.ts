c721de031b942668156499ff324fa2c66c1ca6f9
Peter Talley <peterctalley@gmail.com>
2021-05-24 01:18:44 -0400

    Adjust max scales and edge threshold

--- src/buckling-wf-columns/buckling-wf-columns.ts
+++ src/buckling-wf-columns/buckling-wf-columns.ts
@@ -43,7 +43,7 @@ new App({
             items: [
                 new Model({ name: 'Minor-axis flexural', path: pathToMinorFBMode2 }),
                 new Model({ name: 'Major-axis flexural', path: pathToMajorFB }),
-                new Model({ name: 'Torsional', path: pathToTB, maxScale: 5 }),
+                new Model({ name: 'Torsional', path: pathToTB, maxScale: 3.5 }),
             ]
         },
         {
@@ -69,4 +69,4 @@ new App({
     ],
 });
 
-Model.viewer.edgeThresholdAngle = 40
+Model.viewer.edgeThresholdAngle = 50
