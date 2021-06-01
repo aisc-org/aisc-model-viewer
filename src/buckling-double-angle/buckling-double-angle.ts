60c7cb5509e12306f3e43f5299e7ea8b6ec99853
Peter Talley <peterctalley@gmail.com>
2021-06-01 17:19:33 -0400

    Add commentary and more fun to double angle

--- src/buckling-double-angle/buckling-double-angle.ts
+++ src/buckling-double-angle/buckling-double-angle.ts
@@ -6,6 +6,11 @@ import pathToYaxisFTB from './Double-angle-Y-axis-FTB.glb'
 import pathToLocal from './Double-angle-Local-buckling.glb'
 import pathToSingleAngle from './Double-angle-Single-angle.glb'
 
+// Content
+import commentary from './commentary.md'
+import moreFun from './more-fun.md'
+import './double-angle-shear-center.svg'
+
 new App({
     title: 'Buckling of Double Angle',
     groups: [
@@ -16,6 +21,13 @@ new App({
                 new Model({ name: 'Flexural-torsional buckling', path: pathToYaxisFTB, maxScale: 12 }),
                 new Model({ name: 'Local buckling', path: pathToLocal, maxScale: 2 }),
                 new Model({ name: 'Single-angle buckling', path: pathToSingleAngle }),
+            ]
+        },
+        {
+            name: 'more',
+            items: [
+                new HtmlItem({ name: 'Commentary', content: commentary }),
+                new HtmlItem({ name: 'More fun!', content: moreFun }),
             ]
         },
     ],
