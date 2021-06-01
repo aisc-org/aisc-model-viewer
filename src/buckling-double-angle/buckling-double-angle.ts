5aef7643d43ba8f2135e55520477d51329cd4398
Peter Talley <peterctalley@gmail.com>
2021-06-01 10:45:11 -0400

    Add local and single-angle buckling models to 2L

--- src/buckling-double-angle/buckling-double-angle.ts
+++ src/buckling-double-angle/buckling-double-angle.ts
@@ -3,15 +3,19 @@ import { App, Model, HtmlItem } from '../app';
 // Model files
 import pathToXaxisFB from './Double-angle-X-axis-FB.glb'
 import pathToYaxisFTB from './Double-angle-Y-axis-FTB.glb'
+import pathToLocal from './Double-angle-Local-buckling.glb'
+import pathToSingleAngle from './Double-angle-Single-angle.glb'
 
 new App({
     title: 'Buckling of Double Angle',
     groups: [
         {
-            name: '',
+            name: 'models',
             items: [
                 new Model({ name: 'Flexural buckling', path: pathToXaxisFB }),
                 new Model({ name: 'Flexural-torsional buckling', path: pathToYaxisFTB, maxScale: 12 }),
+                new Model({ name: 'Local buckling', path: pathToLocal, maxScale: 2 }),
+                new Model({ name: 'Single-angle buckling', path: pathToSingleAngle }),
             ]
         },
     ],
